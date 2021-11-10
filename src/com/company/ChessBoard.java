2c21927a43a41cfda046b8e11f2240b6c7664cb1
Noor Ncho <nrandin@gmail.com>
2021-11-10 11:08:36 +0000

    updated chessBoard display

--- src/com/company/ChessBoard.java
+++ src/com/company/ChessBoard.java
@@ -64,7 +64,12 @@ public class ChessBoard {
     }
 
     public void printBoard(){
+        int rowCounter = 0;
+        System.out.println("    0  1  2  3  4  5  6  7");
+        System.out.println("    -----------------------");
+
         for(int row = 0; row < 8; row++){
+            System.out.print(rowCounter + "| ");
             for(int col = 0; col < 8; col++){
                 if(chessBoard[row][col] == null){
                     System.out.print(" _ ");
@@ -72,6 +77,7 @@ public class ChessBoard {
                     System.out.print(" "+ chessBoard[row][col].getId() + " ");
                 }
             }
+            rowCounter++;
             System.out.println();
         }
 
